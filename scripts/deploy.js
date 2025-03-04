064724001186a538754c9144513c863f550782a2
Gato24-tech <ivan.queralt@gmail.com>
2025-03-04 17:30:39 +0100

    Archivo deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    const MyToken = await ethers.getContractFactory("MyToken");
+    const myToken = await MyToken.deploy("MiToken", "MTK");
+
+    await myToken.waitForDeployment();
+
+    console.log(`Contrato desplegado en: ${await myToken.getAddress()}`);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
